b5966c42345d3c957bf99c445f7d84e3839ed6eb
efedincer <dincer.efe@gmail.com>
2024-12-30 01:13:33 +0300

    admin controller added

---
+++ SecureApp/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureApp.Controllers;
+
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
